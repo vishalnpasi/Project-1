f490a5fecc210f2ef68f4438982496781d789a6d
aniket-kh <aniketkhawale333@gmail.com>
2022-09-05 17:26:12 +0530

    author

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const authorsController = require('../controllers/authorsController')
+const authorsController = require('../controllers/authorController')
 const blogsController = require('../controllers/blogsController')
 
 router.post('/authors',authorsController.createAuthor)  // aniket..
 
-router.get('/blogs',blogsController.getBlogs)       // dipanshu
+// router.get('/blogs',blogsController.getBlogs)       // dipanshu
 
-router.put('/blogs/:blogId',blogsController.updateBlogs)    // jay sharma..
+// router.put('/blogs/:blogId',blogsController.updateBlogs)    // jay sharma..
 
-router.delete('/blogs/:blogId',blogsController.deleteBlogsById)
+// router.delete('/blogs/:blogId',blogsController.deleteBlogsById)
 
-router.delete('/blog',blogsController.deleteBlogs)
+// router.delete('/blog',blogsController.deleteBlogs)
 
 module.exports = router;
